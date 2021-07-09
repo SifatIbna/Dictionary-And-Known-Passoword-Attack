c856ac4ebd19e6d7bade20e3aa0fe5b55f127401
Sifat <sifatibna.amin9@gmail.com>
2021-07-09 15:11:54 +0600

    updated script

--- bash.sh
+++ bash.sh
@@ -1,2 +1,2 @@
 gcc -o a.out socket.c
-./a.out 192.168.0.107 5000 POST /api/users/login/test "{\"email\":\"admin@example.com\",\"password\":\"123456\"}" "Content-Type:application/json"
+./a.out 192.168.0.107 5000 POST /api/users/login/test "body" "Content-Type:application/json"
